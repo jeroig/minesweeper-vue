b751dca84e42d7446448b47a893426aa2172246d
jeroig <jeroig@gmail.com>
2020-03-12 18:20:46 -0300

    Login with grid system

--- src/views/Login.vue
+++ src/views/Login.vue
@@ -1,28 +1,34 @@
 <template>
-  <!-- Login Module -->
-  <v-card width="400" class="mx-auto mt-5">
-    <v-card-title>
-      <h1 class="display-1 mx-auto">Login</h1>
-    </v-card-title>
-    <v-card-text>
-      <v-form>
-        <v-text-field label="Username" prepend-icon="mdi-account-circle" />
-        <v-text-field
-          :type="showPassword ? 'text' : 'password'"
-          label="Password"
-          prepend-icon="mdi-lock"
-          :append-icon="showPassword ? 'mdi-eye' : 'mdi-eye-off'"
-          @click:append="showPassword = !showPassword"
-        />
-      </v-form>
-    </v-card-text>
-    <v-divider></v-divider>
-    <v-card-actions>
-      <v-btn color="success">Register</v-btn>
-      <v-spacer></v-spacer>
-      <v-btn color="info">Login</v-btn>
-    </v-card-actions>
-  </v-card>
+  <v-container>
+    <v-row>
+      <v-col>
+        <!-- Login Module -->
+        <v-card width="400" class="mx-auto mt-5" elevation="24">
+          <v-card-title>
+            <h1 class="display-1 mx-auto">Login</h1>
+          </v-card-title>
+          <v-card-text>
+            <v-form>
+              <v-text-field label="Username" prepend-icon="mdi-account-circle" />
+              <v-text-field
+                :type="showPassword ? 'text' : 'password'"
+                label="Password"
+                prepend-icon="mdi-lock"
+                :append-icon="showPassword ? 'mdi-eye' : 'mdi-eye-off'"
+                @click:append="showPassword = !showPassword"
+              />
+            </v-form>
+          </v-card-text>
+          <v-divider></v-divider>
+          <v-card-actions>
+            <v-btn color="success">Register</v-btn>
+            <v-spacer></v-spacer>
+            <v-btn color="info">Login</v-btn>
+          </v-card-actions>
+        </v-card>
+      </v-col>
+    </v-row>
+  </v-container>
 </template>
 
 <script>
