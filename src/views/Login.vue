350df1f4148afb851e79dfb3212962aadbc92268
jeroig <jeroig@gmail.com>
2020-04-20 10:08:12 -0300

    Add "Login" validation

--- src/views/Login.vue
+++ src/views/Login.vue
@@ -4,17 +4,18 @@
       <v-col>
         <!-- Login Module -->
         <v-card width="400" class="mx-auto mt-5" elevation="24">
-          <v-card-title>
-            <h1 class="display-1 mx-auto">Login</h1>
-          </v-card-title>
-          <v-card-text>
-            <v-form>
+          <v-form v-model="isFormValid">
+            <v-card-title>
+              <h1 class="display-1 mx-auto">Login</h1>
+            </v-card-title>
+            <v-card-text>
               <v-text-field
                 label="E-mail"
                 prepend-icon="mdi-account-circle"
                 v-model="email"
                 type="email"
                 name="email"
+                :rules="emailRules"
               />
               <v-text-field
                 :type="showPassword ? 'text' : 'password'"
@@ -24,15 +25,23 @@
                 @click:append="showPassword = !showPassword"
                 v-model="password"
                 name="password"
+                :rules="[v => !!v || 'Password is required']"
               />
-            </v-form>
-          </v-card-text>
-          <v-divider></v-divider>
-          <v-card-actions>
-            <v-btn color="success" v-on:click="register">Register</v-btn>
-            <v-spacer></v-spacer>
-            <v-btn color="info" v-on:click="login">Login</v-btn>
-          </v-card-actions>
+            </v-card-text>
+            <v-divider></v-divider>
+            <v-card-actions>
+              <v-btn
+                color="success"
+                v-on:click="register"
+                :disabled="!isFormValid"
+                >Register</v-btn
+              >
+              <v-spacer></v-spacer>
+              <v-btn color="info" v-on:click="login" :disabled="!isFormValid"
+                >Login</v-btn
+              >
+            </v-card-actions>
+          </v-form>
         </v-card>
       </v-col>
     </v-row>
@@ -46,7 +55,19 @@ export default {
     return {
       showPassword: false,
       email: '',
-      password: ''
+      password: '',
+      isFormValid: false,
+      emailRules: [
+        value => value.indexOf('@') !== 0 || 'Email should have a username.',
+        value => value.includes('@') || 'Email should include an @ symbol.',
+        value =>
+          value.indexOf('.') - value.indexOf('@') > 1 ||
+          'Email should contain a valid domain.',
+        value => value.includes('.') || 'Email should include a period symbol.',
+        value =>
+          value.indexOf('.') <= value.length - 3 ||
+          'Email should contain a valid domain extension.'
+      ]
     }
   },
   methods: {
