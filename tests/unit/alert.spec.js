cccfd816c7ce807627fc59f7e267fee237d3d16d
jeroig <jeroig@gmail.com>
2020-05-22 11:49:08 -0300

    Test 'Alert' component

---
+++ tests/unit/alert.spec.js
@@ -0,0 +1,39 @@
+import Vue from 'vue'
+import Alert from '@/views/Alert.vue'
+//import VueRouter from '@/router'
+import Vuetify from 'vuetify'
+import { createLocalVue, shallowMount } from '@vue/test-utils'
+
+Vue.use(Vuetify)
+
+describe('Alert.vue', () => {
+  let wrapper
+  beforeEach(() => {
+    const localVue = createLocalVue()
+    localVue.use(Vuetify)
+    wrapper = shallowMount(Alert, {
+      localVue,
+      propsData: {
+        message: 'Lorem ipsum',
+        color: 'deep-purple lighten-2',
+        icon: 'mdi-history',
+      },
+    })
+  })
+
+  it('Render the Alert.vue instance', () => {
+    expect(wrapper.isVueInstance()).toBe(true)
+  })
+
+  it('Correct default prop values ', () => {
+    //console.log(wrapper.find('#register').props('disabled'))
+    expect(wrapper.props().message).toBe('Lorem ipsum')
+    expect(wrapper.props().color).toBe('deep-purple lighten-2')
+    expect(wrapper.props().icon).toBe('mdi-history')
+  })
+
+  it('Change props values', async () => {
+    wrapper.setProps({ message: 'bar' })
+    expect(wrapper.props().message).toBe('bar')
+  })
+})
